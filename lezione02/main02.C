d6391e0b00cd51f654d506a7f38e327a5e072ca2
Magistrelli <fabio.magistrelli@studenti.unimi.it>
2020-08-03 12:28:26 +0200

    lezione02: versione definitiva

--- lezione02/main02.C
+++ lezione02/main02.C
@@ -5,17 +5,17 @@
 int main (int argc, char *argv[]){
 
 //Variables
-int M=int(1e4),N=int(1e2);	//number of total RWs and blocks
+int M=int(5e4),N=int(1e2);	//number of total RWs and blocks
 int L=int(M/N);			//number of RWs in each block
-int Nst=int(1e2);		//number of steps
+int Nst=int(1e2)+1;		//number of steps
 double a=1.;			//lattice constant and steps' lenght
 double x0=0.,y0=0.,z0=0.;	//starting point's coordinates
 double x,y,z,r2;		//progressive point's position and dist^2
-DataVett sum(Nst),Zero(Nst);	//progressive sums for single exp, zero vect
+DataVett sum(Nst),Zero(Nst);	//sum all RW in each block, zero vect
 DataVett Sum(Nst),Sum2(Nst);	//progressive sums for blocking method
 DataVett res(Nst),err(Nst);	//means and errors for each time
 DataVett resC(Nst),erC(Nst);	//continuum case
-double sort,ave,ave2,sMed;	//sorted number, averages, stdDev of the mean
+double sort,ave,ave2,sMed;	//random number, averages, stdDev of the mean
 double theta,phi;		//solid angle's variables
 ofstream output;
 
@@ -31,12 +31,12 @@ for(int k=0; k<N; k++){		//repeat for each of N experiments
     for(int j=0; j<L; j++){	//for each experiment L random walks
 	x=x0,y=y0,z=z0;
 	r2=x*x+y*y+z*z;
-	sum.SetComp(0,sum.GetComp(0)+r2);
+	sum.SumComp(0,r2);
 	for(int i=1; i<Nst; i++){	//creating a single RW
-	    sort=rnd.Rannyu(0.,3.);	//sorted from uniform distribution in [0,3] (equal probability for each direction of motion)
+	    sort=rnd.Rannyu(0.,3.);	//from uniform distribution in [0,3] (equal probability for each direction of motion)
 	    if(sort<1.){		//in [0,1] motion in x direction
-		if (sort<0.5)	{x-=a;}	//negative motion in first half of int
-		else		{x+=a;}	//positive motion in second half
+		if (sort<0.5)	{x-=a;}	//negative motion
+		else		{x+=a;}	//positive motion
 	    } else if(sort<2.){		//in [1,2] motion in y direction
 		if (sort<1.5)	{y-=a;}
 		else		{y+=a;}
@@ -44,13 +44,13 @@ for(int k=0; k<N; k++){		//repeat for each of N experiments
 	    else if(sort<2.5)	{z-=a;}
 	    else		{z+=a;}
 	    r2=x*x+y*y+z*z;
-	    sum.SetComp(i,sum.GetComp(i)+r2);
+	    sum.SumComp(i,r2);
 	}
-    }
-    for(int i=0; i<Nst; i++){		//sum of experiments' results
-	ave=sum.GetComp(i)/L;		//sigle block's distance(i)^2
-	Sum.SetComp(i,Sum.GetComp(i)+ave);//sum of dist(i)^2 over experiments
-	Sum2.SetComp(i,Sum2.GetComp(i)+ave*ave);//sum for variance
+    } //ended a block, update global sums for each step
+    for(int i=0; i<Nst; i++){
+	ave=sum.GetComp(i)/L;		//sigle block's mean distance(i)^2
+	Sum.SumComp(i,ave);		//sum of dist(i)^2 over blocks
+	Sum2.SumComp(i,ave*ave);	//sum for variance
     }
 }
 for(int i=0; i<Nst; i++){	//final results
@@ -70,20 +70,20 @@ for(int k=0; k<N; k++){
     for(int j=0; j<L; j++){
 	x=x0,y=y0,z=z0;
 	r2=x*x+y*y+z*z;
-	sum.SetComp(0,sum.GetComp(0)+r2);
+	sum.SumComp(0,r2);
 	    for(int i=1; i<Nst; i++){
 		rnd.SolidAngle(theta,phi);	//uniform sorted solid angle
 		x+=a*sin(theta)*cos(phi);
 		y+=a*sin(theta)*sin(phi);
 		z+=a*cos(theta);
 		r2=x*x+y*y+z*z;
-		sum.SetComp(i,sum.GetComp(i)+r2);
+		sum.SumComp(i,r2);
 	}
     }
     for(int i=0; i<Nst; i++){
 	ave=sum.GetComp(i)/L;
-	Sum.SetComp(i,Sum.GetComp(i)+ave);
-	Sum2.SetComp(i,Sum2.GetComp(i)+ave*ave);
+	Sum.SumComp(i,ave);
+	Sum2.SumComp(i,ave*ave);
     }
 }
 for(int i=0; i<Nst; i++){
